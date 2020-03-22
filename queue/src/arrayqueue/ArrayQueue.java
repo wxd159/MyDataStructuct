19467e14fc1bb458fba5f1a25d035e49c40bf6b1
wangxiaodan <945329681@qq.com>
2020-03-22 13:09:56 +0800

    数组模拟队列测试

--- queue/src/arrayqueue/ArrayQueue.java
+++ queue/src/arrayqueue/ArrayQueue.java
@@ -1,12 +1,14 @@
 package arrayqueue;
 
+import java.util.Scanner;
+
 /**
  * @author wxd
  * @date Created in 2019/12/5
  * @description: 用数组模拟队列
  * @version:1.0
  *
- * 注：该队列是一次性的，在出队后，队空，但不能添加数据，会显示队满
+ * 注：该队列是一次性的，在出队后，队空，但不能添加数据 ，会显示队满
  */
 public class ArrayQueue {
     //数组长度
@@ -29,7 +31,7 @@ public class ArrayQueue {
     }
 
     public boolean isFull(){
-        return rear == maxSize;
+        return rear == maxSize - 1;
     }
 
     //入队
@@ -67,6 +69,62 @@ public class ArrayQueue {
             throw new RuntimeException("队列为空！");
         }else {
             return arr[++front];
+        }
+    }
+}
+
+
+
+class Test{
+    public static void main(String[] args) {
+        ArrayQueue queue = new ArrayQueue(3);
+        char key= ' ';   //接受用户输入
+        Scanner sc = new Scanner(System.in);
+
+        boolean loop = true;
+        while (loop){
+            System.out.println("-----------------------");
+            System.out.println("s(show):");
+            System.out.println("e(exit):退出程序");
+            System.out.println("a(add):入队");
+            System.out.println("g(get):出队");
+            System.out.println("h(head):查看队列头的数据");
+            System.out.println("请输入你的操作");
+
+            key = sc.next().charAt(0);
+
+            switch (key) {
+                case 's':
+                    queue.show();
+                    break;
+                case 'a':
+                    System.out.println("请输入要添加的数据：");
+                    int value = sc.nextInt();
+                    queue.addQueue(value);
+                    break;
+                case 'g':
+                    try {
+                        int res = queue.getQueue();
+                        System.out.printf("取出的数据是：%d\n", res);
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());         //输出异常信息
+                    }
+                    break;
+                case 'h':
+                    try {
+                        int res = queue.headQueue();
+                        System.out.printf("取出的头数据是：%d\n", res);
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case 'e':
+                    sc.close();
+                    loop = false;
+                    break;
+                default:
+                    System.out.println("你的输入有误");
+            }
         }
     }
 }
